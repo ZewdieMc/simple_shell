8ea7b106c8df5663cc898d829f9a660a9f3e4497
EngrMariam <106928141+EngrMariam@users.noreply.github.com>
2022-09-09 19:29:48 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * execute_proc - similar to puts in C
- * @cmd: a pointer the integer we want to set to 402
+ * @cmd: pointer the integer we want to set to 402
  *
  * Return: int
  */
