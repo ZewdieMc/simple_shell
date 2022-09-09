f7da970230bd49cc1b5c6120eaed10f40705afda
EngrMariam <106928141+EngrMariam@users.noreply.github.com>
2022-09-09 19:28:26 +0100

    Update gas.c

--- gas.c
+++ gas.c
@@ -3,7 +3,7 @@
 /**
  * find_command - finds command to execute in path routes.
  *
- * @command: first position of getline input.
+ * @command: the first position of getline input.
  *
  * Return: string of folder for command to be executed.
  **/
